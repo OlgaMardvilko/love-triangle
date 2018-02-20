d30e19a7c9759d1dad23e0699a6c89d5e146efda
OlgaMardvilko <omardvilko@mail.ru>
2018-02-20 14:04:23 +0300

    task two

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let loves = 0, loveTriangle = 0, len = preferences.length;
+  for(let i=0; i<len; i++) {
+    let loveA = preferences[i]-1;
+    let loveB = preferences[loveA]-1;
+    let loveC = preferences[loveB]-1;
+    if(loveC == i){loves++};
+  }
+  loveTriangle = Math.floor(loves/3);
+  return loveTriangle;
 };
